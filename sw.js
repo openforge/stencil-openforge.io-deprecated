9ef5532f67b855f406c82b3e11953b397551b569
Joni Lehto <joni@webjunto.com>
2018-07-20 14:50:18 +0300

    fix: use newer workbox, cache file requests

--- sw.js
+++ sw.js
@@ -1,6 +1,32 @@
-importScripts('workbox-v3.1.0/workbox-sw.js');
+importScripts('workbox-v3.4.1/workbox-sw.js');
 
 self.workbox.skipWaiting();
 self.workbox.clientsClaim();
+
+// your custom service worker code
+
+self.workbox.routing.registerRoute(
+  new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'google-fonts',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200]
+      })
+    ]
+  })
+);
+
+self.workbox.routing.registerRoute(
+  new RegExp('(http|https)://*'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'file-cache',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200]
+      })
+    ]
+  })
+);
 
 self.workbox.precaching.precacheAndRoute([]);
