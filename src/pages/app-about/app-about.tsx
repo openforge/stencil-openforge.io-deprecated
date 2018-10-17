41c796ef8f53b7d19d7ee852ca03cd872edcf4c8
Sam H <sam@Sams-MacBook-Pro.local>
2018-10-17 12:38:09 -0400

    re-doing pull request from cloned repo. added bio for sam  and linked headshots to individual bio pages

--- src/pages/app-about/app-about.tsx
+++ src/pages/app-about/app-about.tsx
@@ -145,6 +145,15 @@ export class AppAbout {
       url: '/team-landing/dhairya-pujara',
     },
     {
+      name: 'Sam Hudgens',
+      image: './../../assets/headshot-sam.png',
+      title: translate('home.member.title.intern'),
+      mail: 'sam@openforge.io',
+      twitter: '',
+      github: 'https://github.com/samhudgens',
+      url: '/team-landing/sam-hudgens',
+    },
+    {
       name: 'YOU?',
       image: './../../assets/headshot-placeholder.jpg',
       title: '',
