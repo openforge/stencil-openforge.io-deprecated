e27c5ac709ca1a642b904cdebe7265a063528099
Sam H <sam@Sams-MacBook-Pro.local>
2019-02-12 11:36:26 -0500

    refactor: delete comments and console logs

--- src/components/app-img/app-img.tsx
+++ src/components/app-img/app-img.tsx
@@ -59,7 +59,6 @@ export class Img {
   private changeImageFormat() {
     if (this.loadSrc && localStorage.getItem('allowWebp') === 'true') {
       const idx = this.loadSrc.lastIndexOf('.');
-      console.log(idx);
       const ext = this.loadSrc.substring(idx + 1, this.loadSrc.length);
       if (ext === 'png' || ext === 'jpg' || ext === 'jpeg') {
         this.loadSrc = `${this.loadSrc.substring(0, idx)}.webp`;
@@ -69,7 +68,6 @@ export class Img {
 
   render() {
     this.changeImageFormat();
-    console.log(this.loadSrc);
     return <img class={{ fit: this.fit }} src={this.loadSrc} alt={this.alt} decoding="async" />;
   }
 }
