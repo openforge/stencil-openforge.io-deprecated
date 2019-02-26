1f39d4ad7b5ff6db01685894c0149032cd6c9ab3
Matt Moran <matt@openforge.io>
2019-02-26 12:50:18 -0500

    feat(blog): added search and pagination to blog posts

--- src/pages/app-blog/app-blog.tsx
+++ src/pages/app-blog/app-blog.tsx
@@ -10,22 +10,69 @@ declare var butter;
 export class AppBlog {
   @Prop({ context: 'isServer' })
   private isServer: boolean;
+
   @State() blogData = [];
+  @State() numberOfPages: number = 0;
+  @State() currentPage: 1;
   @State() blogMeta;
 
   componentWillLoad() {
-    butter.post
-      .list({ page: 1, page_size: 10, exclude_body: true })
-      .then(resp => {
-        this.blogData = [...resp.data.data];
-        this.blogMeta = resp.data.meta;
+    this.getPosts(1);
+  }
 
-        console.log(this.blogData);
-        console.log(this.blogMeta);
-      })
-      .catch(resp => {
-        console.log(resp);
-      });
+  getPosts(page: number, searchQuery?: string) {
+    const pageSize = 2;
+    if (searchQuery) {
+      console.log(searchQuery);
+      butter.post
+        .search(searchQuery)
+        .then(resp => {
+          console.log(resp);
+          this.updateBlogData(resp, page);
+        })
+        .catch(resp => {
+          console.log(resp);
+        });
+    } else {
+      butter.post
+        .list({ page, page_size: pageSize, exclude_body: true })
+        .then(resp => {
+          this.updateBlogData(resp, page);
+        })
+        .catch(resp => {
+          console.log(resp);
+        });
+    }
+  }
+
+  updateBlogData(resp, page) {
+    this.blogData = resp.data.data;
+    this.blogMeta = resp.data.meta;
+    this.numberOfPages = Math.ceil(resp.data.meta.count / 2);
+    this.currentPage = page;
+  }
+
+  handleSearch(event) {
+    this.getPosts(1, event.target.value);
+  }
+
+  handlePaging(event) {
+    switch (event.target.id) {
+      case 'nav-to-next':
+        if (this.blogMeta.next_page) {
+          this.getPosts(this.blogMeta.next_page);
+        }
+        break;
+      case 'nav-to-previous':
+        if (this.blogMeta.previous_page) {
+          this.getPosts(this.blogMeta.previous_page);
+        }
+        break;
+      default:
+        const page = parseInt(event.target.id.substr(-1), 10);
+        this.getPosts(page);
+        break;
+    }
   }
 
   componentDidLoad() {
@@ -39,16 +86,53 @@ export class AppBlog {
   render() {
     let postData = <div>Loading</div>;
     if (this.blogData) {
-      postData = (
-        <div class="blog-container">
-          {this.blogData.map(post => (
-            <div class="card blog-card">
-              <app-blog-card blogPost={post} />
-            </div>
-          ))}
+      postData = this.blogData.map(post => (
+        <div class="card blog-card">
+          <app-blog-card blogPost={post} />
         </div>
+      ));
+    } else if (this.blogData === []) {
+      postData = <div>No posts found</div>;
+    }
+    let pagination = <nav />;
+    if (this.numberOfPages > 1) {
+      const pageNumbers = [...Array(this.numberOfPages)].map((_, i) => {
+        return (
+          <li class="page-item">
+            <a id={`nav-to-page-${i + 1}`} onClick={e => this.handlePaging(e)} class="page-link">
+              {i + 1}
+            </a>
+          </li>
+        );
+      });
+      pagination = (
+        <nav>
+          <ul class="pagination">
+            <li class="page-item">
+              <a id={`nav-to-previous`} onClick={e => this.handlePaging(e)} class="page-link">
+                Previous
+              </a>
+            </li>
+            {pageNumbers}
+            <li class="page-item">
+              <a id={`nav-to-next`} onClick={e => this.handlePaging(e)} class="page-link">
+                Next
+              </a>
+            </li>
+          </ul>
+        </nav>
       );
     }
-    return postData;
+
+    return (
+      <div class="blog-container">
+        <div>
+          <label>Search </label>
+          <input type="text" onKeyUp={e => this.handleSearch(e)} />
+        </div>
+        {postData}
+        {pagination}
+      </div>
+    );
   }
 }
