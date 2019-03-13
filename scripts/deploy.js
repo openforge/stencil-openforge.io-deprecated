2035feac7dd02ab68110710dfe1885f9f1eaa1d7
Matt Moran <matt@openforge.io>
2019-03-13 13:30:24 -0400

    test(deploy): test travis deploy to firebase 7
    test travis deploy to firebase 7

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,44 +7,24 @@ var branch = process.env.TRAVIS_BRANCH,
 var token = process.env.FIREBASE_TOKEN
 
 function deployToFirebase(project) {
-    console.log(process.env);
-    exec("firebase use default && firebase deploy --token " + token, function(error, stdout, stderr) {
-        console.log(error);
-        console.log(stdout);
-        console.log(stderr);
-      });
-
-
-
-    /*
-    return new Promise(function (resolve, reject) {
-        console.log('Deploying to firebase project openforge-'+project);
-
-        var child;
-        if(project === "dev"){
-            child = exec("firebase use default && firebase deploy --token " + token);
-        } else {
-            child = exec("firebase use production && firebase deploy --token " + token);
-        }
-
-        child.stdout.on('data', function (data) {
-            process.stdout.write(data);
+    console.log('Deploying to firebase project openforge-'+project);
+    if(project === 'dev'){
+        exec("firebase use default && firebase deploy --token " + token, function(error, stdout, stderr) {
+            console.log(error);
+            console.log(stdout);
+            console.log(stderr);
         });
-
-        child.on('error', function (data) {
-            process.stdout.write(data);
-            reject('Firebase errored!');
+    } else {
+        exec("firebase use default && firebase deploy --token " + token, function(error, stdout, stderr) {
+            console.log(error);
+            console.log(stdout);
+            console.log(stderr);
         });
-
-        child.on('exit', function () {
-            resolve('Firebase deployed successfully');
-        });
-    });
-    */
+    }
 }
 
 if (
-    (branch == 'develop' ) // && (!pr || pr == 'false') )
+    (branch == 'develop' && (!pr || pr == 'false') )
 ) {
     deployToFirebase('dev');
 } else if (branch == 'master' && (!pr || pr == 'false')) {
