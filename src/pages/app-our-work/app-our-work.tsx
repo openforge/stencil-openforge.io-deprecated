d1593c2a082acd1ae6430413dab25e9e591c3d69
Pablo Huerta <pablo.huerta@udem.edu>
2019-12-04 11:15:36 -0600

    Added footer, cta and started with hero image withoud assets

--- src/pages/app-our-work/app-our-work.tsx
+++ src/pages/app-our-work/app-our-work.tsx
@@ -6,6 +6,21 @@ import { Component } from '@stencil/core';
 })
 export class AppOurWork {
   render() {
-    return <p>Our Work Test</p>;
+    return (
+      <div class="ourwork">
+        <header class="hero">
+          <div class="container">
+            <div class="row align-items-center">
+              <div class="col-12 flex-column text">
+                <h1>Our Work</h1>
+                <h2>Each day we work to make our world better by design. Below you will find examples of projects that we've had the pleasure of working on.</h2>
+              </div>
+            </div>
+          </div>
+        </header>
+        <app-cta />
+        <app-footer />
+      </div>
+    );
   }
 }
