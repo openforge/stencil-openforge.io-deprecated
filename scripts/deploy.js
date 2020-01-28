05f2b3ced38807f07db9f4ca8f85dda70a7efb70
Fernando del olmo <fer.olmo92@gmail.com>
2020-01-28 09:30:01 -0500

    feat(): update deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -9,24 +9,44 @@ var token = process.env.FIREBASE_TOKEN
 function deployToFirebase(project) {
     console.log('Deploying to firebase project openforge-'+project);
     if(project === 'dev'){
+        exec('firebase functions:config:set travis.rebuild_branch="develop"', function(error, stdout, stderr) {
+            console.log(error);
+            console.log(stdout);
+            console.log(stderr);
+        });
         exec("firebase use default && firebase deploy --token " + token, function(error, stdout, stderr) {
             console.log(error);
             console.log(stdout);
             console.log(stderr);
         });
     } else if(project === 'qa') {
+        exec('firebase functions:config:set travis.rebuild_branch="qa"', function(error, stdout, stderr) {
+            console.log(error);
+            console.log(stdout);
+            console.log(stderr);
+        });
         exec("firebase use qa && firebase deploy --token " + token, function(error, stdout, stderr) {
             console.log(error);
             console.log(stdout);
             console.log(stderr);
         });
     } else if(project === 'staging') {
+        exec('firebase functions:config:set travis.rebuild_branch="staging"', function(error, stdout, stderr) {
+            console.log(error);
+            console.log(stdout);
+            console.log(stderr);
+        });
         exec("firebase use staging && firebase deploy --token " + token, function(error, stdout, stderr) {
             console.log(error);
             console.log(stdout);
             console.log(stderr);
         });
     } else if(project === 'prod') {
+        exec('firebase functions:config:set travis.rebuild_branch="master"', function(error, stdout, stderr) {
+            console.log(error);
+            console.log(stdout);
+            console.log(stderr);
+        });
         exec("firebase use production && firebase deploy --token " + token, function(error, stdout, stderr) {
             console.log(error);
             console.log(stdout);
