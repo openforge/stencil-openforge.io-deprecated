e3c0db81c18559bd68fb053d6e1f57f65e4c2cf4
Fernando del olmo <fer.olmo92@gmail.com>
2020-01-28 10:19:47 -0500

    feat(): update deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -9,53 +9,17 @@ var token = process.env.FIREBASE_TOKEN
 function deployToFirebase(project) {
     console.log('Deploying to firebase project openforge-'+project);
     if(project === 'dev'){
-        exec('firebase use default', function(error, stdout, stderr) {
-            console.log(error);
-            console.log(stdout);
-            console.log(stderr);
-        });
-        exec('firebase functions:config:set travis.rebuild_branch="develop"', function(error, stdout, stderr) {
-            console.log(error);
-            console.log(stdout);
-            console.log(stderr);
-        });
-        exec('firebase deploy --token ' + token, function(error, stdout, stderr) {
-            console.log(error);
-            console.log(stdout);
-            console.log(stderr);
-        });
+        exec('firebase use default');
+        exec('firebase functions:config:set travis.rebuild_branch="develop"');
+        exec('firebase deploy --token ' + token);
     } else if(project === 'qa') {
-        exec('firebase use qa', function(error, stdout, stderr) {
-            console.log(error);
-            console.log(stdout);
-            console.log(stderr);
-        });
-        exec('firebase functions:config:set travis.rebuild_branch="qa"', function(error, stdout, stderr) {
-            console.log(error);
-            console.log(stdout);
-            console.log(stderr);
-        });
-        exec('firebase deploy --token ' + token, function(error, stdout, stderr) {
-            console.log(error);
-            console.log(stdout);
-            console.log(stderr);
-        });
+        exec('firebase use qa');
+        exec('firebase functions:config:set travis.rebuild_branch="qa"');
+        exec('firebase deploy --token ' + token);
     } else if(project === 'staging') {
-        exec('firebase use staging', function(error, stdout, stderr) {
-            console.log(error);
-            console.log(stdout);
-            console.log(stderr);
-        });
-        exec('firebase functions:config:set travis.rebuild_branch="staging"', function(error, stdout, stderr) {
-            console.log(error);
-            console.log(stdout);
-            console.log(stderr);
-        });
-        exec('firebase deploy --token ' + token, function(error, stdout, stderr) {
-            console.log(error);
-            console.log(stdout);
-            console.log(stderr);
-        });
+        exec('firebase use staging');
+        exec('firebase functions:config:set travis.rebuild_branch="staging"');
+        exec('firebase deploy --token ' + token);
     } else if(project === 'prod') {
         exec('firebase use production', function(error, stdout, stderr) {
             console.log(error);
